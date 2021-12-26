17d98bebcab45ce0a8c640415fd7ef7949b41bed
Lunga Baliwe <lunga001@gmail.com>
2021-12-26 23:23:51 +0200

    Add other invoice information

--- src/senaite/batch/invoices/batchinvoice/reportview.py
+++ src/senaite/batch/invoices/batchinvoice/reportview.py
@@ -2,19 +2,21 @@
 
 from string import Template
 
-# from bika.lims import api
+from decimal import Decimal
 from senaite.impress import logger
 from senaite.impress.analysisrequest.reportview import ReportView
 
 
-SINGLE_TEMPLATE = Template("""<!-- Batch Invoice Report -->
+SINGLE_TEMPLATE = Template(
+    """<!-- Batch Invoice Report -->
 <div class="report" uids="${uids}" client_uid="${client_uid}">
   <script type="text/javascript">
     console.log("*** BEFORE TEMPLATE RENDER ***");
   </script>
   ${template}
 </div>
-""")
+"""
+)
 
 
 class BatchInvoiceReportView(ReportView):
@@ -22,8 +24,7 @@ class BatchInvoiceReportView(ReportView):
     """
 
     def __init__(self, model, request):
-        logger.info("BatchInvoiceReportView::__init__:model={}"
-                    .format(model))
+        logger.info("BatchInvoiceReportView::__init__:model={}".format(model))
         super(BatchInvoiceReportView, self).__init__(model, request)
         self.model = model
         self.request = request
@@ -46,15 +47,34 @@ class BatchInvoiceReportView(ReportView):
         """
         samples = model_or_collection.instance.getAnalysisRequests()
         data = []
+        batch_data = {
+            "total_subtotal": Decimal("0.0"),
+            "total_discount": Decimal("0.0"),
+            "total_vat": Decimal("0.0"),
+            "total_price": Decimal("0.0"),
+        }
         for sample in samples:
             sample_data = {
-                    "ClientSID": sample.getClientSampleID(),
-                    "SampleID": sample.getId(),
-                    "SampleTypeTitle": sample.getSampleTypeTitle(),
-                    "DateReceived": sample.getDateReceived(),
-                    "Description": sample.description,
-                    "Subtotal": sample.getSubtotal(),
-                    }
+                "ClientSID": sample.getClientSampleID(),
+                "SampleID": sample.getId(),
+                "SampleTypeTitle": sample.getSampleTypeTitle(),
+                "DateReceived": sample.getDateReceived(),
+                "Description": sample.description,
+                "Subtotal": sample.getSubtotal(),
+                "TotalPrice": sample.getTotalPrice(),
+                "Total": sample.getTotal(),
+                "VATAmount": sample.getVATAmount(),
+                "DiscountAmount": sample.getDiscountAmount(),
+                "VAT": self.setup.getVAT(),
+            }
             data.append(sample_data)
+            batch_data["total_subtotal"] += sample.getSubtotal()
+            batch_data["total_discount"] += sample.getDiscountAmount()
+            batch_data["MemberDiscount"] = "{}% Discount".format(
+                self.setup.getMemberDiscount()
+            )
+            batch_data["VAT"] = "{}% VAT".format(self.setup.getVAT())
+            batch_data["total_vat"] += sample.getVATAmount()
+            batch_data["total_price"] += sample.getTotalPrice()
 
-        return data
+        return {"samples": data, "batch_data": batch_data}
