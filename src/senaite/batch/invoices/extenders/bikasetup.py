719c1992626bed1aeae41c3bd282492c87aa611a
Lunga Baliwe <lunga001@gmail.com>
2022-01-02 14:35:00 +0200

    Add Financials field

---
+++ src/senaite/batch/invoices/extenders/bikasetup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from Products.Archetypes.Widget import BooleanWidget
+from Products.CMFCore.permissions import View
+from archetypes.schemaextender.interfaces import IBrowserLayerAwareExtender
+from archetypes.schemaextender.interfaces import ISchemaExtender
+from zope.component import adapts
+from zope.interface import implementer
+
+from bika.lims.fields import ExtBooleanField
+from bika.lims.interfaces import IBikaSetup
+from senaite.batch.invoices import _
+from senaite.batch.invoices.interfaces import ISenaiteBatchInvoicesLayer
+
+financials_field = ExtBooleanField(
+    "Financials",
+    mode="rw",
+    schemata="Accounting",
+    widget=BooleanWidget(
+        label=_(u"Finacials"),
+        description=_(u"Enables batch invoicing functionality"),
+        ))
+
+
+@implementer(ISchemaExtender, IBrowserLayerAwareExtender)
+class BikaSetupSchemaExtender(object):
+    adapts(IBikaSetup)
+    layer = ISenaiteBatchInvoicesLayer
+
+    fields = [
+        financials_field,
+    ]
+
+    def __init__(self, context):
+        self.context = context
+
+    def getOrder(self, schematas):
+        return schematas
+
+    def getFields(self):
+        return self.fields
