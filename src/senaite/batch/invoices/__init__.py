3281fedada7be397252d47c0c32d36d26fd3d446
Lunga Baliwe <lunga001@gmail.com>
2022-01-13 18:44:44 +0200

    Missing from last commit

--- src/senaite/batch/invoices/__init__.py
+++ src/senaite/batch/invoices/__init__.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 """Init and utils."""
+import logging
 from zope.i18nmessageid import MessageFactory
+PRODUCT_NAME = "senaite.batch.invoices"
+PROFILE_ID = "profile-{}:default".format(PRODUCT_NAME)
+logger = logging.getLogger(PRODUCT_NAME)
 
 
 _ = MessageFactory('senaite.batch.invoices')
