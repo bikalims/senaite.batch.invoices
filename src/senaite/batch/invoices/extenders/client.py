c8b8a6360935292b7ffa1c8a6ce645c04d1ceeaa
Lunga Baliwe <lunga001@gmail.com>
2022-01-13 14:25:18 +0200

    Add Client and Laboratory Billing Email Address fields

---
+++ src/senaite/batch/invoices/extenders/client.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from Products.Archetypes.Widget import StringWidget
+from archetypes.schemaextender.interfaces import IBrowserLayerAwareExtender
+from archetypes.schemaextender.interfaces import ISchemaExtender
+from zope.component import adapts
+from zope.interface import implementer
+
+from bika.lims.fields import ExtStringField
+from bika.lims.interfaces import IClient
+from senaite.batch.invoices import _
+from senaite.batch.invoices.interfaces import ISenaiteBatchInvoicesLayer
+
+billing_email_address = ExtStringField(
+    "BillingEmailAddress",
+    mode="rw",
+    schemata="default",
+    widget=StringWidget(
+        label=_(u"Billing Email Address"),
+        description=_(u"Client billing email address"),
+    ),
+    validators=("isEmail", )
+)
+
+
+@implementer(ISchemaExtender, IBrowserLayerAwareExtender)
+class ClientSchemaExtender(object):
+    adapts(IClient)
+    layer = ISenaiteBatchInvoicesLayer
+
+    fields = [
+        billing_email_address,
+    ]
+
+    def __init__(self, context):
+        self.context = context
+
+    def getOrder(self, schematas):
+        return schematas
+
+    def getFields(self):
+        return self.fields
