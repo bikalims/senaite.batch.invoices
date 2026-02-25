19bf0d41047137907708ca5591c1fca9d0b83cb7
Lunga Baliwe <lunga001@gmail.com>
2026-02-25 12:43:24 +0200

    Fixed filtering on invoiced listing

--- src/senaite/batch/invoices/browser/listingview/batchfolder.py
+++ src/senaite/batch/invoices/browser/listingview/batchfolder.py
@@ -49,7 +49,7 @@ class BatchesListingViewAdapter(object):
                         "title": get_image("invoiced.png",
                                            title=t(_("Invoiced"))),
                         "columns": self.listing.columns.keys(),
-                        "contentFilter": {"batch_invoiced_state": "invoiced"}
+                        "contentFilter": {"review_state": "invoiced"}
                         }
             to_be_invoiced = {"id": "uninvoiced",
                               "title": get_image("uninvoiced.png",
